c85d7b36e91d3a932c395d3af5467e6e33879a7c
tiendung311 <109157322+tiendung311@users.noreply.github.com>
2025-04-03 08:27:47 +0700

    modify services manage

--- src/app/admin/services/page.tsx
+++ src/app/admin/services/page.tsx
@@ -4,13 +4,7 @@ import AdminHeader from "@/app/components/AdminHeader";
 import AdminSidebar from "@/app/components/AdminSidebar";
 import styles from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPen,
-  faTrash,
-  faSearch,
-  faRotateLeft,
-} from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface Service {
   id: number;
@@ -20,8 +14,7 @@ interface Service {
 }
 
 export default function ServiceManage() {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [services, setServices] = useState<Service[]>([
+  const services: Service[] = [
     {
       id: 1,
       roomType: "Đơn 3 sao",
@@ -58,60 +51,7 @@ export default function ServiceManage() {
       services: ["Wifi", "TV", "Bồn tắm", "Ban công", "Spa", "Hồ bơi"],
       rooms: [111, 112],
     },
-  ]);
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSearch = () => {
-    const filteredServices = services.filter((service) =>
-      service.roomType.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setServices(filteredServices);
-  };
-
-  const handleReset = () => {
-    setSearchQuery("");
-    setServices([
-      {
-        id: 1,
-        roomType: "Đơn 3 sao",
-        services: ["Wifi", "TV", "Mini Bar"],
-        rooms: [101, 102],
-      },
-      {
-        id: 2,
-        roomType: "Đôi 3 sao",
-        services: ["Wifi", "TV"],
-        rooms: [103, 104],
-      },
-      {
-        id: 3,
-        roomType: "Đơn 4 sao",
-        services: ["Wifi", "TV", "Bồn tắm"],
-        rooms: [105, 106],
-      },
-      {
-        id: 4,
-        roomType: "Đôi 4 sao",
-        services: ["Wifi", "TV", "Bồn tắm", "Ban công"],
-        rooms: [107, 108],
-      },
-      {
-        id: 5,
-        roomType: "Đơn 5 sao",
-        services: ["Wifi", "TV", "Bồn tắm", "Ban công", "Spa"],
-        rooms: [109, 110],
-      },
-      {
-        id: 6,
-        roomType: "Đôi 5 sao",
-        services: ["Wifi", "TV", "Bồn tắm", "Ban công", "Spa", "Hồ bơi"],
-        rooms: [111, 112],
-      },
-    ]);
-  };
+  ];
 
   return (
     <div className={styles.dashboardContainer}>
@@ -120,27 +60,6 @@ export default function ServiceManage() {
         <AdminHeader />
         <div className={styles.content}>
           <h2 className={styles.title}>Quản lý dịch vụ</h2>
-
-          <div className={styles.searchContainer}>
-            <input
-              type="text"
-              className={styles.searchInput}
-              placeholder="Tìm kiếm..."
-              value={searchQuery}
-              onChange={handleSearchChange}
-            />
-            <button className={styles.searchButton} onClick={handleSearch}>
-              <FontAwesomeIcon icon={faSearch} style={{ marginRight: 10 }} />
-              Tìm kiếm
-            </button>
-            <button className={styles.resetButton} onClick={handleReset}>
-              <FontAwesomeIcon
-                icon={faRotateLeft}
-                style={{ marginRight: 10 }}
-              />
-              Làm mới
-            </button>
-          </div>
 
           <div className={styles.tableContainer}>
             <table className={styles.table}>
